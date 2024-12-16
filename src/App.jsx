3689e5d4a2518fffda68d09c8264b830189511e3
asadbekumarov <asadbekumarov922@gmail.com>
2024-12-17 00:24:06 +0500

    end

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Skleton from "./components/skleton/Skleton";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
